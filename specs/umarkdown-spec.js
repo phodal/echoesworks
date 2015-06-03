fe9950ebcdeaf75550b00814f5bac30698676d8b
Fengda HUANG <h@phodal.com>
2015-06-03 23:06:20 +0800

    Add more tests

--- specs/umarkdown-spec.js
+++ specs/umarkdown-spec.js
@@ -1,4 +1,4 @@
-describe("Main", function () {
+describe("Markdown", function () {
 	beforeEach(function () {
 
 	});
@@ -13,7 +13,17 @@ describe("Main", function () {
 	});
 
 	it("should parse link", function () {
-		expect(EW.md.parse("[phodal](http://www.phodal.com/)")).toBe('\n<a class="mmd_phodalcom" href="http://www.phodal.com/">phodal</a>\n\n');
+		expect(EW.md.parse("[phodal](http://www.phodal.com/)"))
+			.toBe('\n<a class="mmd_phodalcom" href="http://www.phodal.com/">phodal</a>\n\n');
+
+		expect(EW.md.parse("![phodal](http://www.phodal.com/favicon.ico)"))
+			.toBe('\n<img src="http://www.phodal.com/favicon.ico" alt="phodal" title="phodal" />\n\n');
+
+		expect(EW.md.parse("<h@phodal.com>")).toBe('\n<a href="mailto:h@phodal.com">h@phodal.com</a>\n');
+
+		expect(EW.md.parse("<http://www.phodal.com>"))
+			.toBe('\n<a class="mmd_phodalcom" href="http://www.phodal.com">www.phodal.com</a>\n');
+
 		expect(EW.md.parse("[phodal][1]\n[1]: http://www.phodal.com/"))
 			.toBe('\n<a class="mmd_phodalcom" href="http://www.phodal.com/">phodal</a>\n\n');
 	});
@@ -41,6 +51,25 @@ describe("Main", function () {
 	it("should parse code", function () {
 		expect(EW.md.parse('```\nvar md   = document.getElementById("md").value```'))
 			.toBe('<code>\nvar&nbsp;md&nbsp;&nbsp;&nbsp;=&nbsp;document.getElementById("md").value</code>\n\n');
+	});
+
+	it("should parse ul list", function () {
+		expect(EW.md.parse('* this\n* is a\n* list'))
+			.toBe('<ul><li>this</li>\n<li>is a</li>\n<li>list</li>\n</ul>\n');
+
+		expect(EW.md.parse('1. this\n2. is a'))
+			.toBe('<ol><li>this</li>\n<li>is a</li>\n</ol>\n');
+
+		expect(EW.md.parse('* this \n* is a\n  1. test\n  1. and\n  1. demo\n* list'))
+			.toBe('<ul><li>this </li>\n<li>is a</li>\n<ol><li>test</li>\n<li>and</li>\n<li>demo</li>\n</ol><li>list</li>\n</ul>\n');
+	});
+
+	it("should parse ul table", function () {
+		expect(EW.md.parse('this | is a   | table\n-----|--------|--------\nwith | sample | content'))
+			.toBe('<table><tr><th>this</th><th>is a</th><th>table</th></tr><tr><td>with</td><td>sample</td><td>content</td></tr>\n</table>');
+
+		expect(EW.md.parse('this | is a   | table\n-----:|:--------:|:--------\nwith | sample | content'))
+			.toBe('<table><tr><th align="right">this</th><th align="center">is a</th><th align="left">table</th></tr><tr><td align="right">with</td><td align="center">sample</td><td align="left">content</td></tr>\n</table>');
 	});
 
 });
