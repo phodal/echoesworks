6a4a933d915c0a252f71f324e3bc08e50105ca15
Fengda HUANG <h@phodal.com>
2015-06-04 20:46:01 +0800

    Add event handler test

--- specs/event_handler-spec.js
+++ specs/event_handler-spec.js
@@ -1,6 +1,8 @@
 describe("Main", function () {
 	it("should return true", function () {
-		expect(EW()).toBeTruthy();
+		expect(EW.handleInput(9)).toBeTruthy();
+		expect(EW.handleInput(33)).toBeTruthy();
+		expect(EW.handleInput(38)).toBeTruthy();
 	});
 });
 
