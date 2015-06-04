0173e923c190a1c091424d1bf8517dda6add5061
Fengda HUANG <h@phodal.com>
2015-06-04 08:50:26 +0800

    Add keyboard event handler

--- dist/echoesworks.js
+++ dist/echoesworks.js
@@ -20,9 +20,44 @@ EchoesWorks.VERSION = '0.0.0';
 root.EchoesWorks = EchoesWorks;
 root.EW = EchoesWorks;
 
+var api = {
+  prev: function (){
+    console.log("prev");
+  },
+  next: function (){
+    console.log("next");
+  }
+};
+
+document.addEventListener("keydown", function ( event ) {
+  if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+    event.preventDefault();
+  }
+}, false);
+
+document.addEventListener("keyup", function ( event ) {
+  if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+    switch( event.keyCode ) {
+      case 33: // pg up
+      case 37: // left
+      case 38: // up
+        api.prev();
+        break;
+      case 9:  // tab
+      case 32: // space
+      case 34: // pg down
+      case 39: // right
+      case 40: // down
+        api.next();
+        break;
+    }
+
+    event.preventDefault();
+  }
+}, false);
 
 /*
- * µmarkdown.js
+ * micro-markdown.js
  * markdown in under 5kb
  *
  * Copyright 2014, Simon Waldherr - http://simon.waldherr.eu/
