2943646c46f48e3e0e5aa4f2bb25cee5280a25d4
Fengda HUANG <h@phodal.com>
2015-06-04 21:52:08 +0800

    Add impress.js license

--- src/api.js
+++ src/api.js
@@ -1,3 +1,18 @@
+/**
+ * impress.js
+ *
+ * impress.js is a presentation tool based on the power of CSS3 transforms and transitions
+ * in modern browsers and inspired by the idea behind prezi.com.
+ *
+ *
+ * Copyright 2011-2012 Bartek Szopka (@bartaz)
+ *
+ * Released under the MIT and GPL Licenses.
+ *
+ * ------------------------------------------------
+ * https://github.com/bartaz/impress.js/blob/6314e2e15012c9db69f76e77a8bb1c437a7312b8/js/impress.js
+ */
+	
 var steps = [1 ,3] , activeStep, api;
 api = {
 	goto: function () {
