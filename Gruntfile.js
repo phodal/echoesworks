dfe917a67d0c63e865fb21a4f1c262362c0e36a3
Fengda HUANG <h@phodal.com>
2015-06-07 22:16:37 +0800

    format gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,100 +1,100 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
+	grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: '.'
-        }
-      }
-    },
+		pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			server: {
+				options: {
+					port: 8000,
+					base: '.'
+				}
+			}
+		},
 
-    jasmine : {
-      src : 'dist/echoesworks.js',
-      options : {
-        host: "http://0.0.0.0:8000",
-        vendor: ['node_modules/jasmine-ajax/lib/mock-ajax.js'],
-        specs : 'specs/*-spec.js',
-        template: require('grunt-template-jasmine-istanbul'),
-        templateOptions: {
-          coverage: 'coverage/coverage.json',
-          report: {
-            type: 'lcov',
-            options: {
-              dir: 'coverage'
-            }
-          },
-          thresholds: {
-            lines: 80,
-            statements: 80,
-            branches: 50,
-            functions: 50
-          }
-        }
-      }
-    },
+		jasmine: {
+			src: 'dist/echoesworks.js',
+			options: {
+				host: "http://0.0.0.0:8000",
+				vendor: ['node_modules/jasmine-ajax/lib/mock-ajax.js'],
+				specs: 'specs/*-spec.js',
+				template: require('grunt-template-jasmine-istanbul'),
+				templateOptions: {
+					coverage: 'coverage/coverage.json',
+					report: {
+						type: 'lcov',
+						options: {
+							dir: 'coverage'
+						}
+					},
+					thresholds: {
+						lines: 80,
+						statements: 80,
+						branches: 50,
+						functions: 80
+					}
+				}
+			}
+		},
 
-    concat: {
-      options: {
-        separator: "\n\n"
-      },
-      dist: {
-        src: [
-          'src/_intro.js',
-          'src/main.js',
-          'src/utils.js',
-          'src/ajax.js',
-          'src/slide.js',
-          'src/parser.js',
-          'src/umarkdown.js',
-          'src/_outro.js',
-          'src/events_handler.js'
-        ],
-        dest: 'dist/<%= pkg.name.replace(".js", "") %>.js'
-      }
-    },
+		concat: {
+			options: {
+				separator: "\n\n"
+			},
+			dist: {
+				src: [
+					'src/_intro.js',
+					'src/main.js',
+					'src/utils.js',
+					'src/ajax.js',
+					'src/slide.js',
+					'src/parser.js',
+					'src/umarkdown.js',
+					'src/_outro.js',
+					'src/events_handler.js'
+				],
+				dest: 'dist/<%= pkg.name.replace(".js", "") %>.js'
+			}
+		},
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name.replace(".js", "") %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name.replace(".js", "") %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name.replace(".js", "") %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'dist/<%= pkg.name.replace(".js", "") %>.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		},
 
-    jshint: {
-      files: ['dist/echoesworks.js'],
-      options: {
-        globals: {
-          console: true,
-          module: true,
-          document: true
-        },
-        jshintrc: '.jshintrc'
-      }
-    },
+		jshint: {
+			files: ['dist/echoesworks.js'],
+			options: {
+				globals: {
+					console: true,
+					module: true,
+					document: true
+				},
+				jshintrc: '.jshintrc'
+			}
+		},
 
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['concat', 'jshint', 'qunit']
-    }
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['concat', 'jshint', 'qunit']
+		}
 
-  });
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('test', ['jshint','connect', 'jasmine']);
-  grunt.registerTask('default', ['concat','connect', 'jshint','jasmine', 'uglify' ]);
+	grunt.registerTask('test', ['jshint', 'connect', 'jasmine']);
+	grunt.registerTask('default', ['concat', 'connect', 'jshint', 'jasmine', 'uglify']);
 
 };
