c6789ff6d39868cd231edbc000c45fcf15d0c258
Fengda HUANG <h@phodal.com>
2015-06-08 09:20:46 +0800

    Add timer parser

--- specs/parser-spec.js
+++ specs/parser-spec.js
@@ -8,9 +8,9 @@ describe("Parser", function () {
 		jasmine.Ajax.uninstall();
 	});
 
+	var ew = new EchoesWorks({element: 'slide', source: 'data/data.json'});
+
 	it("parse data", function () {
-		var ew = new EchoesWorks({element: 'slide', source: 'data/data.json'});
-		//
 		spyOn(ew.parser, 'parse');
 		ew.parser.init('/data/data.json');
 		//jasmine.clock().tick(300);
@@ -19,14 +19,21 @@ describe("Parser", function () {
 		jasmine.Ajax.requests.mostRecent().respondWith({
 			"status": 200,
 			"contentType": 'text/plain',
-			"responseText": '[{"time": "00:05.51",' +
-			'"code": "https://raw.githubusercontent.com/phodal/echoesworks/master/bower.json","word": "hello, world, next"}]'
+			"responseText": '[{"time": "00:05.51","code": "https://raw.githubusercontent.com/phodal/echoesworks/master/bower.json","word": "hello, world, next"}]'
 		});
 	});
 
-	it("parse time", function () {
-		var ew = new EchoesWorks({element: 'slide', source: 'data/data.json'});
+	it("parse data correctly", function () {
+		var data = [{
+			"time": "00:05.51",
+			"code": "https://raw.githubusercontent.com/phodal/echoesworks/master/bower.json",
+			"word": "hello, world, next"
+		}];
+		var result = ew.parser.parse(data);
+		expect(result[2][0]).toBe("hello, world, next");
+	});
 
+	it("parse time correctly", function () {
 		var result = ew.parser.parseTime(["00:05.51"]);
 		expect(result[0][0]).toBe(5.5);
 	});
