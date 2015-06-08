15f59d1abb2525a62edf0e39afb704a23a74835f
Fengda HUANG <h@phodal.com>
2015-06-08 08:57:25 +0800

    add time parser

---
+++ specs/parser-spec.js
@@ -0,0 +1,64 @@
+describe("Ajax", function () {
+    beforeEach(function () {
+        jasmine.Ajax.install();
+    });
+    afterEach(function () {
+        jasmine.Ajax.uninstall();
+    });
+
+    it("specifying response when you need it", function (done) {
+        var doneFn = jasmine.createSpy("success");
+
+        EchoesWorks.get('/some/cool/url', function (result) {
+            expect(result).toEqual("awesome response");
+            done();
+        });
+
+        expect(jasmine.Ajax.requests.mostRecent().url).toBe('/some/cool/url');
+        expect(doneFn).not.toHaveBeenCalled();
+
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            "status": 200,
+            "contentType": 'text/plain',
+            "responseText": 'awesome response'
+        });
+    });
+
+    it("specifying html when you need it", function (done) {
+        var doneFn = jasmine.createSpy("success");
+
+        EchoesWorks.load('/some', function (result) {
+            expect(result).toEqual("<h2>fsasfA</h2>");
+            done();
+        });
+
+        expect(jasmine.Ajax.requests.mostRecent().url).toBe('/some');
+        expect(doneFn).not.toHaveBeenCalled();
+
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            "status": 200,
+            "contentType": 'text/plain',
+            "responseText": '<h2>fsasfA</h2>'
+        });
+    });
+
+
+    it("should be post to some where", function (done) {
+        var doneFn = jasmine.createSpy("success");
+
+        EchoesWorks.post('/some/cool/url', [], function (result) {
+            expect(result).toEqual("awesome response");
+            done();
+        });
+
+        expect(jasmine.Ajax.requests.mostRecent().url).toBe('/some/cool/url');
+        expect(doneFn).not.toHaveBeenCalled();
+
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            "status": 200,
+            "contentType": 'text/plain',
+            "responseText": 'awesome response'
+        });
+    });
+});
+
