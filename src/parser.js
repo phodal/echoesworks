e567aa96bf6759da06487772ca33ffd51fe83c99
Fengda HUANG <h@phodal.com>
2015-06-08 08:51:39 +0800

    add time parser

--- src/parser.js
+++ src/parser.js
@@ -31,6 +31,19 @@ parser.parse = function (data) {
 
 	data.forEach(callback);
 	console.log(times, codes, words);
+	console.log(parser.parseTime(times));
+};
+
+parser.parseTime = function(times) {
+	var pattern = /\[\d{2}:\d{2}.\d{2}\]/g,
+		result = [];
+
+	times.forEach(function(v1) {
+		var t = v1.slice(1, -1).split(':');
+		var value = v1.replace(pattern, '');
+		result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value]);
+	});
+	return result;
 };
 
 EchoesWorks.prototype = EchoesWorks.extend(EchoesWorks.prototype, {parser: parser});
