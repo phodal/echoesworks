f739abc629787e3e47afbc03e4e2b6bd818145d4
Fengda HUANG <h@phodal.com>
2015-06-09 09:02:51 +0800

    add test to basic play logic

--- specs/main-spec.js
+++ specs/main-spec.js
@@ -1,7 +1,40 @@
 describe("Main", function () {
-	it("should return true", function () {
-		var EW = new EchoesWorks();
+	var EW = new EchoesWorks();
+
+	beforeEach(function () {
+		jasmine.clock().install();
+	});
+	afterEach(function () {
+		jasmine.clock().uninstall();
+	});
+
+	it("should return element", function () {
 		expect(EW.element).toBe('#slide');
 	});
+
+	it("should return respond logic", function () {
+		EW.play();
+		expect(EW.playing).toBeTruthy();
+		EW.pause();
+		expect(EW.playing).toBeFalsy();
+		EW.play();
+		EW.stop();
+		expect(EW.playing).toBeFalsy();
+	});
+
+	it("should auto call update function", function () {
+		var ew = new EchoesWorks();
+		spyOn(ew, 'update');
+		expect(ew.update).not.toHaveBeenCalled();
+		jasmine.clock().tick(2000);
+		expect(ew.update).toHaveBeenCalled();
+	});
+
+	it("should test time correctly", function () {
+		var ew = new EchoesWorks();
+		jasmine.clock().tick(2000);
+		expect(ew.time).toBeGreaterThan(2);
+	});
+
 });
 
