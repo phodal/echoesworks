6b1587848a9a8eba96d2df8ba71b24747de4891e
Fengda HUANG <h@phodal.com>
2015-06-14 23:10:10 +0800

    Add test for 100 width

--- specs/bar-spec.js
+++ specs/bar-spec.js
@@ -10,13 +10,22 @@ describe("Process Bar", function () {
 		jasmine.clock().uninstall();
 	});
 
-	it("should return element", function () {
+	it("should return correct process width", function () {
 		bar.go(30);
 		jasmine.clock().tick(100);
 		expect(bar.bars[0].moving).toBe(true);
 		jasmine.clock().tick(400);
 		expect(bar.bars[0].here).toBe(30);
 		expect(bar.bars[0].moving).toBe(false);
+	});
+
+	it("should return 0 when go 100 ", function () {
+		bar.go(99);
+		jasmine.clock().tick(1000);
+		expect(bar.bars[0].here).toBe(99);
+		bar.go(100);
+		jasmine.clock().tick(600);
+		expect(bar.bars[0].here).toBe(0);
 	});
 
 });
